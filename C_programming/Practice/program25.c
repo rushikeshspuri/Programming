c2f24178e941d0000624d05dbf7184b10fed9842
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-05-30 15:32:57 +0530

    Add files via upload
    Practice of my Building logic in C

---
+++ C_programming/Practice/program25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;       // loop counter
+
+    // Iteration
+    
+    for(iCnt = 1; iCnt <= 4; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }  
+
+    return 0;
+}
