f28f4a98cf2f081501a88650fc9733bbeb90f74e
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-06-08 01:20:48 +0530

    Add files via upload

---
+++ C_programming/Practice/program128.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool LinearSearch(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == 11)
+        {
+            return true;        // Bad Programming
+        }
+    }
+    return false;
+     
+}
+
+int main ()
+{
+    int iLenght = 0, iCnt = 0;          // Store number of elements;
+    int *Brr = NULL;                 // Pointer 
+    bool bRet = false;
+    
+    printf("Enter Number of Elements\n");
+    scanf("%d",&iLenght);             // To take input from User in iSize
+
+    Brr = (int*)malloc(sizeof(int) * iLenght);      // Dynamic memory Alloction
+
+    printf("Enter the Elements\n");
+
+    for(iCnt = 0; iCnt < iLenght; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);             // Value intialized in Brr & Array Created
+    }
+
+    bRet = LinearSearch(Brr,iLenght);
+
+    if(bRet == true)
+    {
+        printf("Element is present\n");
+    }
+    else
+    {
+        printf("Element is not present\n");
+    }
+
+    free(Brr);
+    
+    return 0;
+}
---
+++ C_programming/Practice/program130.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool LinearSearch(int Arr[], int iSize, int iNo)
+{
+    int iCnt = 0;
+    bool bFlag = false;
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            bFlag = true;
+            break;        
+        }
+    }
+        return bFlag;
+     
+}
+
+int main ()
+{
+    int iLenght = 0, iCnt = 0 , iValue = 0;       
+    int *Brr = NULL;                 // Pointer 
+    bool bRet = false;
+    
+    printf("Enter Number of Elements\n");
+    scanf("%d",&iLenght);             // To take input from User in iSize
+
+    Brr = (int*)malloc(sizeof(int) * iLenght);      // Dynamic memory Alloction
+
+    printf("Enter the Elements\n");
+
+    for(iCnt = 0; iCnt < iLenght; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);             // Value intialized in Brr & Array Created
+    }
+
+    printf("Enter the elements that you want to search : \n");
+    scanf("%d",&iValue);
+
+    bRet = LinearSearch(Brr,iLenght,iValue);
+    
+    if(bRet == true)
+    {
+        printf("Element is present\n");
+    }
+    else
+    {
+        printf("Element is not present\n");
+    }
+
+    free(Brr);
+    
+    return 0;
+}
