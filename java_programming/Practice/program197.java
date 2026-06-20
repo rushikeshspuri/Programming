a6f6c69a66ea0816d53133147b169ecda78dffd5
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-06-21 01:46:14 +0530

    Add files via upload

---
+++ java_programming/Practice/program197.java
@@ -0,0 +1,45 @@
+/*
+    iRow = 4
+    iCol = 4
+
+    $   $   $   $
+    $   $   $   $
+    $   $   $   $
+    $   $   $   $
+*/
+
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow , int iCol)
+    {
+        int i = 0 , j = 0;
+        for(i = 1; i<= iRow; i++)
+        {
+            for(j = 1; j<= iCol; j++)
+            {
+                System.out.print("$\t");
+            }
+            System.out.println();
+        }
+    }
+}
+class program197
+{
+    public static void main(String A[]) 
+    { 
+        int iValue1 = 0, iValue2 = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of rows");
+        iValue1 =  sobj.nextInt();
+
+        System.out.println("Enter number of column");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+
+        sobj.close();
+    }
+}
