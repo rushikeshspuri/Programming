8296a270a1f9bde10bcc69028d94b4b00092905c
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-06-22 19:29:50 +0530

    Update program71.c

--- C_programming/Practice/program71.c
+++ C_programming/Practice/program71.c
@@ -6,7 +6,7 @@ int main ()
     
     iCnt = 1;           // Intialization 1
     while(iCnt <= 5)    // Condition 2
-    {
+    { 
         printf("Jay Ganesh...\n");  // Loop Body 4
         iCnt++;         // Displacement 3
 
