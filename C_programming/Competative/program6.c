21e7bf94af1db87a95a10b2fe2236b9518d297eb
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-06-23 20:21:49 +0530

    Update program6.c

--- C_programming/Competative/program6.c
+++ C_programming/Competative/program6.c
@@ -9,7 +9,7 @@
 //
 //  Function name : Display
 //  Input   : Integer
-//  Output  : None
+//  Output  : None 
 //  Description : Check whether number is Divisible by 5
 //  Date : 24/05/2026
 //  Author : Rushikesh Puri
