341d143c79dbc282c06733a435447f4f2a0da4fc
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-06-23 20:23:01 +0530

    Add files via upload

---
+++ C_programming/Practice/program231.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    char str [] = {'J','a','y',' ','G','a','n','e','s','h'};
+
+    printf("%s\n",str);
+
+    return 0;
+}
+
+// garbage after jay ganesh?? because no \0 
