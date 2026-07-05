dc4737c0746e86a27eb2f2b99d723da19fa91152
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-07-05 23:06:45 +0530

    Add files via upload

---
+++ C_programming/Practice/program328.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+typedef unsigned int UINT;
+
+int main()
+{
+    // Smallest Value of int 
+    UINT iMask = 0x00000000;
+    printf("%d\t%X\n",iMask,iMask);
+    
+    return 0;
+}
