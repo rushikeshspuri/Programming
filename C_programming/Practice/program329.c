548306afab11f5615fb75ab70d1237c87db6bf41
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-07-05 23:07:10 +0530

    Add files via upload

---
+++ C_programming/Practice/program329.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+typedef unsigned int UINT;
+
+int main()
+{
+    // Largest Value of int 
+    UINT iMask = 0xFFFFFFFF;
+    printf("%u\t%X\n",iMask,iMask);
+    
+    return 0;
+}
