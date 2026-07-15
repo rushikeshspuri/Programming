d69259cf41f1d615f3ba17933fd0bbd15e255c8d
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-07-16 00:00:02 +0530

    Add files via upload

--- C_programming/Competitive/program1.c
+++ C_programming/Competitive/program1.c
@@ -1,50 +1,8 @@
-/////////////////////////////////////////////////////
-//
-//  Include required header files
-//
-////////////////////////////////////////////////////
-#include <stdio.h>
+#include<stdio.h>
 
-////////////////////////////////////////////////////
-//
-//  Function name : Divide 
-//  Input   : Integer
-//  Output  : Integer
-//  Description : Perform Division of 2 numbers
-//  Date : 22/05/2026
-//  Author : Rushikesh Puri
-//
-////////////////////////////////////////////////////
-
-int Divide (int iNo1 , int iNo2)
+int main()
 {
-    int iAns = 0;
-
-    if (iNo2 == 1)
-    {
-        return -1;
-    }
-    
-    iAns = iNo1/iNo2;
-
-    return iAns;
-
-}
-/////////////////////////////////////////////////////
-//
-//  Application to perform Division of 2 numbers
-//
-/////////////////////////////////////////////////////
-
-int main ()
-{
-    int iValue1 = 15 , iValue2 = 5;
-    int iRet = 0;
-
-    iRet = Divide(iValue1,iValue2);
-
-    printf("Division is %d\n",iRet);
+    printf("Jay Ganesh...\n");
 
     return 0;
-    
 }
--- C_programming/Competitive/program4.c
+++ C_programming/Competitive/program4.c
@@ -1,65 +1,18 @@
-/////////////////////////////////////////////////////
-//
-//  Include required header files
-//
-////////////////////////////////////////////////////
 #include<stdio.h>
 
-typedef int Bool;
-#define True 1 
-#define False 0
-
-////////////////////////////////////////////////////////////
-//
-//  Function name : Check
-//  Input   : Integer
-//  Output  : Boolean
-//  Description : Check whether number is Divisible by 5
-//  Date : 24/05/2026
-//  Author : Rushikesh Puri
-//
-///////////////////////////////////////////////////////////
-
-Bool Check(int iNo)
+int main()
 {
-    if ((iNo % 5) == 0)
-    {
-        return True ;
-    }
-    else 
-    {
-        return False;
-    }
-}
-///////////////////////////////////////////////////////////////
-//
-//  Application to Check whether number is Divisible by 5
-//
-///////////////////////////////////////////////////////////////
+    int i,j,k;
 
-int main ()
-{
-    int iValue = 0;     
-    Bool bRet = False;
+    printf("Enter first number : \n");
+    scanf("%d",&i);
 
-    printf("Enter your number");
-    scanf("%d",&iValue);
+    printf("Enter second number : \n");
+    scanf("%d",&j);
+
+    k = i + j;
+
+    printf("Addition is : %d\n",k);
     
-    bRet = Check(iValue);
-
-    if (bRet == True)
-    {
-        printf("Divisble by 5");
-    }
-    else
-    {
-        printf("Not divible by 5");
-    }
     return 0;
 }
-//////////////////////////////////////////////////////////////////
-//
-// Input  : Integer
-// Output : Divisble by 5 or Not Divisble by 5
-//
-//////////////////////////////////////////////////////////////////
