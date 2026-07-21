d2035aa35e8dd786a7eaaddb81c6dfac4d412435
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-07-22 01:19:11 +0530

    Add files via upload

---
+++ C++_Programming/Competitive/program213.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+int FirstOccurence(T *arr,int iSize,T iNo)
+{
+    int i = 0;
+    int Index = -1;
+    
+    if(iSize < 1)
+    {
+        printf("Invalid Input");
+    }
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(arr[i] == iNo)
+        {
+           return i ;
+        }
+    }
+
+   return -1;
+}
+
+int main()
+{
+    int iRet = 0;
+
+    int Arr[] = {10,20,30,10,40,10,30,40,10};
+    
+    iRet = FirstOccurence(Arr,10,40);
+
+    cout<<iRet;
+
+    return 0;
+}
