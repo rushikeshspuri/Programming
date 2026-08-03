6955ac2dab24efaba75ee8eb64f24bb6ae42ab9e
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-03 13:31:35 +0530

    Add files via upload

---
+++ C++_Programming/Practice/program488.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+class ArrayX
+{
+    public:
+        T *Arr;
+        int Size;
+
+        ArrayX(int No)
+        {
+            Size = No;
+            Arr =  new T[Size];
+        }
+
+        ~ArrayX()
+        {
+            delete []Arr;
+        }
+
+        void Accept()
+        {
+            int i = 0;
+
+            cout<<"Enter the Elements : \n";
+            
+            for(i = 0; i < Size; i++)
+            {
+                cin>>Arr[i];
+            }
+        }
+
+        void Display()
+        {
+            int i = 0;
+
+            cout<<"Elements of the array are : \n";
+            
+            for(i = 0; i < Size; i++)
+            {
+                cout<<Arr[i]<<"\n";
+            }
+        }
+
+        T Summation()
+        {
+            int i = 0;
+            T Sum = 0;
+            
+            for(i = 0; i < Size; i++)
+            {
+                Sum = Sum + Arr[i];
+            }
+
+            return Sum;
+        }
+
+
+        
+};
+
+int main()
+{
+
+    ArrayX <int>aobj(5);
+    
+    aobj.Accept();
+    aobj.Display();
+
+    cout<<"Summmation is : "<<aobj.Summation()<<"\n";
+    
+    return 0;
+}
