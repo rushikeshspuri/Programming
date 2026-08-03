97140b9b00e7e1ff6f33eb798613f0eafb70dca9
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-03 13:39:41 +0530

    Add files via upload

---
+++ C++_Programming/Practice/program493.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+template <class T>
+struct node
+{
+    T data;
+    struct node<T>* next;
+};
+
+template <class T>
+class Queue
+{
+    private:
+        struct node<T>* first;
+        int iCount;
+
+    public:
+        Queue();
+
+        void Enqueue(T iNo);         // InsertLast
+        T Dequeue ();                 // DeleteFirst
+
+        void Display();
+        int Count();
+
+};
+
+template <class T>
+Queue<T> :: Queue()
+{
+    this->first = NULL;
+    this->iCount = 0;
+}
+
+template <class T>
+void Queue<T> :: Enqueue(T iNo)
+{
+    struct node<T>* newn = NULL;
+    struct node<T>* temp = NULL;
+
+    newn = new struct node<T>();
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(this -> first == NULL)
+    {
+        this->first = newn;
+    }
+    else
+    {
+        temp = this -> first;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+
+        temp -> next = newn;
+    }
+
+    this -> iCount++;
+   
+}       
+
+template <class T>
+T Queue<T> :: Dequeue()
+{
+    T iValue = 0;
+    struct node<T> * temp = NULL;
+
+    if(this -> first == NULL)
+    {
+        cout<<"Queue is empty..."<<"\n";
+        return -1;
+    }
+    else
+    {
+        iValue = this -> first -> data;
+        temp = this -> first;
+
+        this -> first = this -> first -> next;
+        delete temp;
+
+        this -> iCount--;
+        return iValue;
+    }
+    
+}
+           
+template <class T>
+void Queue<T> :: Display()
+{
+    struct node<T> * temp = NULL;
+
+    temp = this -> first;
+    
+    while(temp != NULL)
+    {
+        cout<<"| "<<temp -> data<<" |\n";
+        temp = temp -> next;
+    }
+
+}
+
+template <class T>
+int Queue<T> :: Count()
+{
+    return this -> iCount;
+}
+
+int main()
+{
+    Queue <int>sobj;
+    int iRet = 0;
+
+    sobj.Enqueue(11);
+    sobj.Enqueue(21);
+    sobj.Enqueue(51);
+    sobj.Enqueue(101);
+
+    sobj.Display();
+    
+    iRet = sobj.Count();
+    
+    cout<<"Elements of Queue are "<<iRet<<"\n";
+    
+    iRet = sobj.Dequeue();
+    
+    cout<<"Removed Element is : "<<iRet<<"\n";
+    
+    sobj.Display();
+    
+    iRet = sobj.Count();
+    
+    cout<<"Elements of Queue are "<<iRet<<"\n";
+
+
+    return 0;
+}
