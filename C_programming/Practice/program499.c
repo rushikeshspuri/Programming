794f8d6fb674b80df54ebb7a8911b612c2812fc7
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-04 21:36:56 +0530

    Add files via upload

---
+++ C_programming/Practice/program499.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// itteration for approach
+void Display()
+{
+    int i = 0;
+    for(i = 1; i <= 4 ; i++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+
+int main()
+{  
+    Display();
+
+    return 0;
+}
