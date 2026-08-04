a50c6a1184bf15feaeb7731446b0775b7a4c679b
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-04 21:36:33 +0530

    Add files via upload

---
+++ C_programming/Practice/program498.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+// sequence approach
+void Display()
+{
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+}
+
+int main()
+{  
+    Display();
+
+    return 0;
+}
