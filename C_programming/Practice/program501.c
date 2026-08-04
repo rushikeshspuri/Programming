5db994e1e7e7739d7fd828d9920133e91efe1e3a
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-04 21:37:42 +0530

    Add files via upload

---
+++ C_programming/Practice/program501.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+// itteration while approach
+
+void Display()
+{
+    int i = 0;
+    
+    i = 1;
+
+    while(  i <= 4 )
+    {
+        printf("Jay Ganesh...\n");
+        i++;
+    }
+}
+
+int main()
+{  
+    Display();
+
+    return 0;
+}
