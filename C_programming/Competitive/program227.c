4aaccb4c28fa9e2e84149f16ca38f878537c97b1
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-05 21:17:28 +0530

    Add files via upload

---
+++ C_programming/Competitive/program227.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int Sum(int iNo)
+{
+    static int iDigit = 0;
+    static int iSum = 0;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iSum = iSum + iDigit;
+        iNo = iNo / 10;
+
+        Sum(iNo);
+    }
+
+    return iSum;
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    iRet = Sum(iValue);
+
+    printf("Summation of Digits : %d\n",iRet);
+    
+    return 0;
+}
