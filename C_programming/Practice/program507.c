fc3d1acb86182f0d8cbf341f5d95a7fd7de61605
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-09 17:01:07 +0530

    Add files via upload

---
+++ C_programming/Practice/program507.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// Recurssion infinite approach
+void Display()
+{
+    int i = 1;
+
+    while (1)
+    {
+        printf("Jay Ganesh...%d\n",i);
+        i++;
+    }
+       
+}
+
+int main()
+{  
+    Display();
+
+    return 0;
+}
