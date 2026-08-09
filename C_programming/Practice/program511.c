56bffc33d2742738b12c9f12f000f9b55fcdbf2e
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-09 17:03:11 +0530

    Add files via upload

---
+++ C_programming/Practice/program511.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// using recurrsion
+void Display()
+{
+    static int i = 1;
+
+    if(  i <= 4 )
+    {
+        printf("Jay Ganesh...%d\n",i);
+        i++;
+        Display();
+    }
+}
+
+int main()
+{  
+    Display();
+
+    return 0;
+}
