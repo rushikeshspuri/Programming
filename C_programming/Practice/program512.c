dba482d8c3a94905a4ed31afab4b3cc5ae46f34e
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-09 17:03:32 +0530

    Add files via upload

---
+++ C_programming/Practice/program512.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int i = 0;
+    
+    i = 1;
+
+    while(  i <= iNo )
+    {
+        printf("Jay Ganesh...\n");
+        i++;
+    }
+}
+
+int main()
+{  
+    
+    Display(7);
+
+    return 0;
+}
