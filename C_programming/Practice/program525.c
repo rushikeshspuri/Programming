c78cd4b7198e9a715e8be8fa989ca5cf4ee92d05
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-10 22:54:32 +0530

    Add files via upload

---
+++ C_programming/Practice/program525.c
@@ -0,0 +1,38 @@
+// 4
+// 1 + 2 + 3 + 4 = 10
+
+
+#include<stdio.h>
+
+int Summation(int iNo)
+{
+    static int iSum = 0;
+    
+    if(iNo != 0)
+    {
+        iSum = iSum + iNo;
+        
+        iNo--;
+
+        Summation(iNo);    
+    }
+
+    return iSum;
+}
+
+int main()
+{  
+    int iValue = 0;
+    int iRet = 0;
+    
+    printf("Enter the Number\n");
+    
+    scanf("%d",&iValue);
+    
+    iRet = Summation(iValue);
+    
+    printf("Summation is : %d\n",iRet);
+
+
+    return 0;
+}
