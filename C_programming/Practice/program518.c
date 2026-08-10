e67014f2199aa9dbba2409fe92b3599ff8afd11b
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-10 22:51:53 +0530

    Add files via upload

---
+++ C_programming/Practice/program518.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+// using recurrsion
+void Display(int iNo)
+{
+
+    if( iNo != 0 )
+    {
+        printf("Jay Ganesh...%d\n",iNo);
+        Display(iNo--); // issue post decrement
+    }
+}
+
+int main()
+{  
+    int iValue = 0;
+    
+    printf("Enter the frequency\n");
+    
+    scanf("%d",&iValue);
+    
+    Display(iValue);
+
+    printf("End of main\n");
+
+    return 0;
+}
