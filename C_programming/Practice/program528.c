9fb9ef6cd66bd2057aee455832d1b1a143913658
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-12 01:42:15 +0530

    Add files via upload

---
+++ C_programming/Practice/program528.c
@@ -0,0 +1,33 @@
+// 4
+// 4 * 3 * 2 * 1 = 24
+
+#include<stdio.h>
+
+unsigned long int Factorial(int iNo)
+{
+    unsigned long int iFact = 1;
+
+    while(iNo != 0)
+    {
+        iFact = iFact * iNo;
+        iNo--;
+    }
+
+    return iFact;
+}
+
+int main()
+{  
+    int iValue = 0;
+    unsigned long int iRet = 0;
+    
+    printf("Enter the frequency\n");
+    scanf("%d",&iValue);
+    
+    iRet = Factorial(iValue);
+    
+    printf("Factorial is : %lu\n",iRet);
+
+
+    return 0;
+}
