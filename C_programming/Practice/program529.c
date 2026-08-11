2256c9b0b229403205a5086ac26bd9e79602c04c
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-12 01:42:35 +0530

    Add files via upload

---
+++ C_programming/Practice/program529.c
@@ -0,0 +1,35 @@
+// 4
+// 4 * 3 * 2 * 1 = 24
+
+#include<stdio.h>
+
+typedef unsigned long int ULONG;
+
+ULONG Factorial(int iNo)
+{
+    ULONG iFact = 1;
+
+    while(iNo != 0)
+    {
+        iFact = iFact * iNo;
+        iNo--;
+    }
+
+    return iFact;
+}
+
+int main()
+{  
+    int iValue = 0;
+    ULONG iRet = 0;
+    
+    printf("Enter the Number\n");
+    scanf("%d",&iValue);
+    
+    iRet = Factorial(iValue);
+    
+    printf("Factorial is : %lu\n",iRet);
+
+
+    return 0;
+}
