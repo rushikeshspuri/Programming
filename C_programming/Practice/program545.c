52f28411e36f07e7cb527f29cbca08cafaed4d84
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-16 12:11:37 +0530

    Add files via upload

---
+++ C_programming/Practice/program545.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int StrlenX(char *str)
+{
+    int iCount = 0;
+    
+    while(*str != '\0')
+    {
+        iCount++;
+        str++;
+    }
+
+    return iCount;
+}
+
+int main()
+{  
+    char Arr[20] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter the string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = StrlenX(Arr);
+    printf("string length is : %d\n",iRet);
+
+    return 0;
+}
