747156c52336e32cb2fa7b116463509920d4af49
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-24 15:23:13 +0530

    Add files via upload

---
+++ C++_Programming/Practice/program565.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Searching
+{
+    private:
+        int iSize;
+        int *Arr;
+    
+    public:
+        Searching(int iNo);
+        ~Searching();
+};
+
+Searching :: Searching(int iNo)
+{
+    iSize = iNo;
+    Arr = new int[iSize];
+}
+
+Searching :: ~Searching()
+{
+    delete []Arr;
+}
+
+int main()
+{
+
+    Searching sobj(5);
+
+    return 0;
+}
