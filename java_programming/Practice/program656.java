fe6fda519bb7e1d8d0f577cf00e65fabde5ec17e
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-25 23:55:56 +0530

    Add files via upload

---
+++ java_programming/Practice/program656.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class program656
+{
+    public static void main(String A[] ) 
+    {  
+        try
+        {
+            File fobj = new File("Demo.txt");
+
+            fobj.createNewFile();      
+        }
+        catch(IOException iobj)
+        {
+            System.out.println(iobj.getMessage());
+        }
+        catch(Exception eobj)
+        {
+            System.out.println(eobj.getMessage());
+        }
+               
+    }
+}
