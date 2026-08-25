a2b25d06d8b112b4e83f513a38f38ed3028f6469
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-25 23:57:07 +0530

    Add files via upload

---
+++ java_programming/Practice/program660.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+class program660
+{
+    public static void main(String A[] ) 
+    {  
+        File fobj = null;
+        boolean bRet = false;
+        
+        try
+        {
+            fobj = new File("Demo.txt");
+
+            bRet = fobj.exists();
+
+            if(bRet == true)
+            {
+                System.out.println("File already exists");
+            }
+            else
+            {
+                fobj.createNewFile();   
+                System.out.println("File gets successfully created");
+            }
+           
+        }
+        catch(IOException iobj)
+        {
+            System.out.println(iobj.getMessage());
+        }
+        catch(Exception eobj)
+        {
+            System.out.println(eobj.getMessage());
+        }
+               
+    }
+}
